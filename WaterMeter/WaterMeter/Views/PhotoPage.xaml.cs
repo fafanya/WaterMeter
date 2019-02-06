6a5dcb28c97535a33d5bfdf7d84f66fe4e200dbe
eugene afanasovich <eugeneafanasovich@gmail.com>
2019-02-07 01:50:28 +0300

    Add project files.

---
+++ WaterMeter/WaterMeter/Views/PhotoPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using Plugin.Permissions;
+using Plugin;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions.Abstractions;
+
+namespace WaterMeter.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PhotoPage : ContentPage
+	{
+		public PhotoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void TakePhoto_Clicked(object sender, System.EventArgs e)
+        {
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+            if (cameraStatus != PermissionStatus.Granted || storageStatus != PermissionStatus.Granted)
+            {
+                if (cameraStatus != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Camera permission", "Camera permission", "OK");
+                    }
+                }
+
+                if (storageStatus != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Storage permission", "Storage permission", "OK");
+                    }
+                }
+
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+                cameraStatus = results[Permission.Camera];
+                storageStatus = results[Permission.Storage];
+            }
+
+
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+            if (file == null)
+                return;
+            await DisplayAlert("File Location", file.Path, "OK");
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
+}
